395cb591828bc15507cc4b9fdc32ca7d6fd74338
neill <neill@192.168.1.73>
2020-12-29 11:18:07 +0000

    Pushing changes to add further security

--- src/main/java/com/neill/workouttracker/security/WebSecurityConfig.java
+++ src/main/java/com/neill/workouttracker/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.neill.workouttracker.service.UserDetailsServiceImpl;
 
@@ -63,7 +64,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
         .defaultSuccessUrl("/workouts", true)
         .permitAll()
         .and()
-        .logout().permitAll();
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login?logout").invalidateHttpSession(true).deleteCookies("JSESSIONID").permitAll();
     }
 
 }
